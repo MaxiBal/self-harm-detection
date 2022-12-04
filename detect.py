5ec475b64a7736ef81bca64c758db693eaab61ab
Maxi B <860281@lcps.org>
2022-12-03 23:00:00 -0500

    updated detect

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 # Self-Harm Detection
 
 import matplotlib
+import math
 from nltk.tokenize import sent_tokenize, word_tokenize
 import numpy as np
 import pandas as pd
@@ -14,15 +15,11 @@ from sklearn.linear_model import LogisticRegression
 DATA_FILE_PATH = r'C:\Users\860281\Downloads\Suicide_Detection.csv'
 
 data = pd.read_csv(DATA_FILE_PATH)
-df = pd.DataFrame(data, columns=['text', 'class'])
+df = pd.DataFrame(data, columns=['text', 'class']).to_numpy()
+
+df[:, 1][df[:, 1] == 'suicide'] = 1
+df[:, 1][df[:, 1] == 'non-suicide'] = 0
 
 vectorizor = CountVectorizor()
 train_x = vectorizor.fit_transform(df).toarray()
 
-for ep in range(n_epochs):
-    avg_cost = 0
-
-    for i in range(len(train_x)):
-        data_points = train_x[i]
-        label = train_y[i]
-        pred_prob = sigmoid()
