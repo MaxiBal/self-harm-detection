4cf9374711dd067e0024c8284347537271176bc8
MaxiBal <maxibal@baltel.com>
2022-12-04 11:05:44 -0500

    add logreg

--- detect.py
+++ detect.py
@@ -22,8 +22,12 @@ df = pd.DataFrame(data, columns=['text', 'class'])
 df['class'][df['class'] == 'suicide'] = 1
 df['class'][df['class'] == 'non-suicide'] = 0
 
-X = df['text']
-Y = df['class']
+df['class'] = df['class'].astype(float)
+
+X = df['text'].str.encode('utf-8').str.decode('latin-1').astype(str)
+Y = df['class'].to_numpy()
+
+print('Vectorizing Text')
 
 count_vect = CountVectorizer()
 x_train_counts = count_vect.fit_transform(X)
@@ -32,10 +36,11 @@ tfidf_transformer = TfidfTransformer()
 x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=16)
+x_train, x_test, y_train, y_test = train_test_split(x_train_tfidf, Y, test_size=0.25, random_state=16)
 
+print('Starting Logistic Regression')
 
-log_reg = LogisticRegression(verbose=1, solver='liblinear', random_state=0, C=5, penalty='12', max_iter=1000)
+log_reg = LogisticRegression(verbose=1, solver='liblinear', random_state=0, C=5, penalty='l2', max_iter=1000)
 model = log_reg.fit(x_train, y_train)
 
 y_score = model.predict(x_test)
