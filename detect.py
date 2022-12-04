850c07a189b9ec2a3a73699c4a084a30086d83aa
MaxiBal <maxibal@baltel.com>
2022-12-04 00:10:42 -0500

    add regression

--- detect.py
+++ detect.py
@@ -1,25 +1,49 @@
 # Self-Harm Detection
 
-import matplotlib
-import math
-from nltk.tokenize import sent_tokenize, word_tokenize
+import logging
+
 import numpy as np
 import pandas as pd
 
-from sklearn.feature_extraction.text import CountVectorizor
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfTransformer
 
 # Import data
 
 DATA_FILE_PATH = r'.\Suicide_Detection.csv'
 
-data = pd.read_csv(DATA_FILE_PATH)
-df = pd.DataFrame(data, columns=['text', 'class']).to_numpy()
+print("Reading data file.")
 
-df[:, 1][df[:, 1] == 'suicide'] = 1
-df[:, 1][df[:, 1] == 'non-suicide'] = 0
+data = pd.read_csv(DATA_FILE_PATH, on_bad_lines='skip')
+df = pd.DataFrame(data, columns=['text', 'class'])
 
-vectorizor = CountVectorizor()
-train_x = vectorizor.fit_transform(df).toarray()
+df['class'][df['class'] == 'suicide'] = 1
+df['class'][df['class'] == 'non-suicide'] = 0
+
+X = df['text']
+Y = df['class']
+
+count_vect = CountVectorizer()
+x_train_counts = count_vect.fit_transform(X)
+
+tfidf_transformer = TfidfTransformer()
+x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
+
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=16)
+
+
+log_reg = LogisticRegression(verbose=1, solver='liblinear', random_state=0, C=5, penalty='12', max_iter=1000)
+model = log_reg.fit(x_train, y_train)
+
+y_score = model.predict(x_test)
+
+n_right = 0
+for i in range(len(y_score)):
+    if y_score[i] == y_test[i]:
+        n_right += 1
+
+print("Accuracy: %.2f%%" % ((n_right/float(len(y_test)) * 100)))
 
