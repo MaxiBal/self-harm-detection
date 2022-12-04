5ed8cb0a0c8acd09191bf1deccff9e6ac66922b9
MaxiBal <maxibal@baltel.com>
2022-12-04 11:27:33 -0500

    finished logistic regression and started app

--- detect.py
+++ detect.py
@@ -13,42 +13,68 @@ from sklearn.feature_extraction.text import TfidfTransformer
 # Import data
 
 DATA_FILE_PATH = r'.\Suicide_Detection.csv'
+def read_data(DATA_FILE_PATH):
+    print("Reading data file.")
 
-print("Reading data file.")
+    data = pd.read_csv(DATA_FILE_PATH, on_bad_lines='skip')
+    df = pd.DataFrame(data, columns=['text', 'class'])
 
-data = pd.read_csv(DATA_FILE_PATH, on_bad_lines='skip')
-df = pd.DataFrame(data, columns=['text', 'class'])
+    df['class'][df['class'] == 'suicide'] = 1
+    df['class'][df['class'] == 'non-suicide'] = 0
 
-df['class'][df['class'] == 'suicide'] = 1
-df['class'][df['class'] == 'non-suicide'] = 0
+    df['class'] = df['class'].astype(float)
 
-df['class'] = df['class'].astype(float)
+    X = df['text'].str.encode('utf-8').str.decode('latin-1').astype(str)
+    Y = df['class'].to_numpy()
 
-X = df['text'].str.encode('utf-8').str.decode('latin-1').astype(str)
-Y = df['class'].to_numpy()
+    nan_output = np.isnan(Y)
 
-print('Vectorizing Text')
+    Y[nan_output] = 0
 
-count_vect = CountVectorizer()
-x_train_counts = count_vect.fit_transform(X)
+    return X, Y
 
-tfidf_transformer = TfidfTransformer()
-x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
+def vectorize_text(X, Y):
+    print('Vectorizing Text')
+
+    count_vect = CountVectorizer()
+    x_train_counts = count_vect.fit_transform(X)
+
+    tfidf_transformer = TfidfTransformer()
+    x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 
 
-x_train, x_test, y_train, y_test = train_test_split(x_train_tfidf, Y, test_size=0.25, random_state=16)
+    x_train, x_test, y_train, y_test = train_test_split(x_train_tfidf, Y, test_size=0.25, random_state=16)
 
-print('Starting Logistic Regression')
+    return x_train, x_test, y_train, y_test
 
-log_reg = LogisticRegression(verbose=1, solver='liblinear', random_state=0, C=5, penalty='l2', max_iter=1000)
-model = log_reg.fit(x_train, y_train)
+def logistic_regression(x_train, y_train):
 
-y_score = model.predict(x_test)
+    print('Starting Logistic Regression')
 
-n_right = 0
-for i in range(len(y_score)):
-    if y_score[i] == y_test[i]:
-        n_right += 1
+    log_reg = LogisticRegression(verbose=1, solver='liblinear', random_state=0, C=5, penalty='l2', max_iter=1000)
+    model = log_reg.fit(x_train, y_train)
 
-print("Accuracy: %.2f%%" % ((n_right/float(len(y_test)) * 100)))
+    return model
 
+def test_model(model, x_test, y_test):
+    y_score = model.predict(x_test)
+
+    n_right = 0
+    for i in range(len(y_score)):
+        if y_score[i] == y_test[i]:
+            n_right += 1
+
+    print("Accuracy: %.2f%%" % ((n_right/float(len(y_test)) * 100)))
+
+def train_model():
+
+    X, Y = read_data(DATA_FILE_PATH)
+    x_train, x_test, y_train, y_test = vectorize_text(X, Y)
+    model = logistic_regression(x_train, y_train)
+    test_model(model, x_test, y_test)
+
+    return model
+
+
+if __name__ == '__main__':
+    train_model()
